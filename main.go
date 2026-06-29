b186f2e4be5891a1998d82084ff5985712367fb2
yukikim <tki6ra@gmail.com>
2026-06-29 13:43:43 +0900

    GET /todos を作成

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -23,7 +24,25 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "OK")
 	})
+	http.HandleFunc("/todos", getTodosHandler)
 
 	fmt.Println("server started at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("server error:", err)
+	}
+}
+
+func getTodosHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	todoList := make([]Todo, 0, len(todos))
+	for _, todo := range todos {
+		todoList = append(todoList, todo)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todoList)
 }
