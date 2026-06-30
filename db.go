69748dbf4a73597ff38a46c3ef9c432e531a75fc
yukikim <tki6ra@gmail.com>
2026-06-30 17:45:07 +0900

    DB接続に関するコメントを追記した

--- db.go
+++ db.go
@@ -7,18 +7,18 @@ import (
 )
 
 func openDB(databaseURL string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", databaseURL)
-	if err != nil {
+	db, err := sql.Open("postgres", databaseURL) // データベースに接続するためのsql.DBオブジェクトを作成
+	if err != nil {                              // データベース接続に失敗した場合はエラーを返す
 		return nil, err
 	}
 
-	if err := db.Ping(); err != nil {
-		db.Close()
+	if err := db.Ping(); err != nil { // データベース接続が有効かどうかを確認するためにPingメソッドを呼び出す
+		db.Close() // 接続が無効な場合はデータベース接続を閉じる
 		return nil, err
 	}
 
-	if err := createTodosTable(db); err != nil {
-		db.Close()
+	if err := createTodosTable(db); err != nil { // todosテーブルが存在しない場合は作成する
+		db.Close() // テーブル作成に失敗した場合はデータベース接続を閉じる
 		return nil, err
 	}
 
@@ -26,6 +26,7 @@ func openDB(databaseURL string) (*sql.DB, error) {
 }
 
 func createTodosTable(db *sql.DB) error {
+	// todosテーブルが存在しない場合は作成するためのSQL文を実行
 	_, err := db.Exec(`
 		CREATE TABLE IF NOT EXISTS todos (
 			id SERIAL PRIMARY KEY,
